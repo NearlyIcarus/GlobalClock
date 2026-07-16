4f43df17322642e5463a132d9b857f16f361af7a
NearlyIcarus <brady.minich1@gmail.com>
2026-07-16 13:31:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,32 @@
 const CONFIG = {
+
     title: "GLOBAL TIME",
+
     subtitle: "Reference Time Zones",
 
-    regions: {
-        Americas: [
-            ...
-        ],
-        Australia: [
-            ...
-        ]
-    }
+    regions: [
+
+        {
+            name: "AMERICAS",
+
+            zones: [
+                { label: "Eastern", timezone: "America/New_York" },
+                { label: "Central", timezone: "America/Chicago" },
+                { label: "Pacific", timezone: "America/Los_Angeles" },
+                { label: "Honolulu", timezone: "Pacific/Honolulu" }
+            ]
+        },
+
+        {
+            name: "AUSTRALIA",
+
+            zones: [
+                { label: "Western Australia", timezone: "Australia/Perth" },
+                { label: "Darwin", timezone: "Australia/Darwin" },
+                { label: "Sydney", timezone: "Australia/Sydney" }
+            ]
+        }
+
+    ]
+
 };
