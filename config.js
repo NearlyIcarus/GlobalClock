fe8663f7268faeed329298ac856dc2945e25639a
NearlyIcarus <brady.minich1@gmail.com>
2026-07-16 13:46:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
-    title: "GLOBAL TIME",
+    title: "GLOBAL OPERATIONS TIME",
 
     subtitle: "Reference Time Zones",
 
@@ -8,25 +8,55 @@ const CONFIG = {
 
         {
             title: "AMERICAS",
-            element: "americas",
 
             zones: [
-                { label: "Eastern", timezone: "America/New_York" },
-                { label: "Central", timezone: "America/Chicago" },
-                { label: "Pacific", timezone: "America/Los_Angeles" },
-                { label: "Honolulu", timezone: "Pacific/Honolulu" }
+
+                {
+                    label: "Eastern",
+                    timezone: "America/New_York"
+                },
+
+                {
+                    label: "Central",
+                    timezone: "America/Chicago"
+                },
+
+                {
+                    label: "Pacific",
+                    timezone: "America/Los_Angeles"
+                },
+
+                {
+                    label: "Honolulu",
+                    timezone: "Pacific/Honolulu"
+                }
+
             ]
+
         },
 
         {
             title: "AUSTRALIA",
-            element: "australia",
 
             zones: [
-                { label: "Western Australia", timezone: "Australia/Perth" },
-                { label: "Darwin", timezone: "Australia/Darwin" },
-                { label: "Sydney", timezone: "Australia/Sydney" }
+
+                {
+                    label: "Western Australia",
+                    timezone: "Australia/Perth"
+                },
+
+                {
+                    label: "Darwin",
+                    timezone: "Australia/Darwin"
+                },
+
+                {
+                    label: "Sydney",
+                    timezone: "Australia/Sydney"
+                }
+
             ]
+
         }
 
     ]
